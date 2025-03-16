f0a15ca18a6fdc78ba2b248ca9b202ec1ab47e25
Jay-Asalways <>
2025-03-16 15:20:06 +0700

    fix : fixing ui on search box

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import "boxicons";
 
 function ArticleSection() {
   return (
@@ -10,12 +11,21 @@ function ArticleSection() {
         </h1>
       </div>
       <div className="flex flex-col p-4 gap-4">
-        <Input type="text" placeholder="Search" />
+        <div className="relative">
+          <Input type="text" placeholder="Search" />
+          <box-icon className="absolute right-2 top-2" name="search"></box-icon>
+        </div>
         <div className="flex flex-col gap-1">
           <Label className="text-[16px]" htmlFor="text">
             Category
           </Label>
-          <Input type="text" id="category" placeholder="Highlight" />
+          <div className="relative">
+            <Input type="text" id="category" placeholder="Highlight" />
+            <box-icon
+              className="absolute right-2 top-2"
+              name="chevron-down"
+            ></box-icon>
+          </div>
         </div>
       </div>
     </>
